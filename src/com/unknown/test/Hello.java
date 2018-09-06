e470db6a427a88167952eb701b991292da3c7b8c
snbrathnayake <snbrathnayake@gmail.com>
2018-09-05 18:29:16 -0700

    init commit

---
+++ src/com/unknown/test/Hello.java
@@ -0,0 +1,10 @@
+package com.unknown.test;
+
+public class Hello {
+    public static void main(String [] args){
+
+        for (int i = 0; i < 5 ; i++) {
+            System.out.println(i + ": Hello");
+        }
+    }
+}
