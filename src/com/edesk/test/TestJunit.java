feb5252f37502212911e56e481d616ffb4651153
snbrathnayake <snbrathnayake@gmail.com>
2018-09-09 10:39:24 +0530

    added test

---
+++ src/com/edesk/test/TestJunit.java
@@ -0,0 +1,16 @@
+package com.edesk.test;
+import org.junit.Test;
+import static  org.junit.Assert.*;
+
+
+public class TestJunit {
+
+    Hello  hello = new Hello();
+
+    @Test
+    public void testTotal() {
+        String res = hello.verifire(4);
+        assertEquals(res , "Even");
+    }
+
+}
