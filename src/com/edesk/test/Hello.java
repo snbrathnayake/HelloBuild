e2ff16927b2942d4c0f85fb02e03b46d671aa69b
snbrathnayake <snbrathnayake@gmail.com>
2018-09-09 10:17:11 +0530

    fd

---
+++ src/com/edesk/test/Hello.java
@@ -0,0 +1,10 @@
+package com.edesk.test;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        String result = new Identifire().verifire(5);
+        System.out.println("Enter number is : " + result);
+    }
+
+}
