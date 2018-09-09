5b1b4f252c62355839897d0d6f272c7101081618
snbrathnayake <snbrathnayake@gmail.com>
2018-09-09 10:22:12 +0530

    fd

--- src/com/edesk/test/Hello.java
+++ src/com/edesk/test/Hello.java
@@ -3,8 +3,16 @@ package com.edesk.test;
 public class Hello {
 
     public static void main(String[] args) {
-        String result = new Identifire().verifire(5);
+        String result = verifire(5);
         System.out.println("Enter number is : " + result);
     }
 
+    public static String verifire(int number) {
+
+        if (number % 2 == 0) {
+            return "Even";
+        } else {
+            return "Odd";
+        }
+    }
 }
