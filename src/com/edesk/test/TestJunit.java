59ad9d02f0bae65d9f90f77faa7f6d9e28e0e81d
snbrathnayake <snbrathnayake@gmail.com>
2018-09-09 11:26:35 +0530

    fd

--- src/com/edesk/test/TestJunit.java
+++ src/com/edesk/test/TestJunit.java
@@ -1,15 +1,17 @@
 package com.edesk.test;
+
+import com.edesk.test.*;
 import org.junit.Test;
 import static  org.junit.Assert.*;
 
 
 public class TestJunit {
 
-    Hello  hello = new Hello();
+    Hello hello = new Hello();
 
     @Test
     public void testTotal() {
-        String res = hello.verifire(4);
+        String res = Hello.verifire(4);
         assertEquals(res , "Even");
     }
 
